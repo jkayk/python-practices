716bc3ad35aefcd33073fe5ef28d8131d175f65b
jkayk <jirehkang10@gmail.com>
2025-03-25 21:17:32 +0900

    clarify assignments, update readme

--- assignments/build-a-scraper/extractors/berlinstartupjobs.py
+++ assignments/build-a-scraper/extractors/berlinstartupjobs.py
@@ -37,13 +37,3 @@ def extract_berlinstartupjobs(keyword):
 
     return jobs_db
 
-"""
-def save_to_file(file_name, jobs):
-    file = open(f"{file_name}_jobs.csv", "w")
-    file.write("Title, Company, Description, URL\n")
-    for job in jobs:
-        file.write(f"{job['title']}, {job['company']}, {job['description']}, {job['link']}\n")
-    file.close()
-
-save_to_file("python", extract_berlinstartupjobs("python"))
-"""
--- assignments/build-a-scraper/extractors/weworkremotely.py
+++ assignments/build-a-scraper/extractors/weworkremotely.py
@@ -35,13 +35,3 @@ def extract_weworkremotely(keyword):
 
     return jobs_db
 
-"""
-def save_to_file(file_name, jobs):
-    file = open(f"{file_name}_jobs.csv", "w")
-    file.write("Title, Company, Region, URL\n")
-    for job in jobs:
-        file.write(f"{job['title']}, {job['company']}, {job['region']}, {job['link']}\n")
-    file.close()
-
-save_to_file("react", extract_weworkremotely("react"))
-"""
